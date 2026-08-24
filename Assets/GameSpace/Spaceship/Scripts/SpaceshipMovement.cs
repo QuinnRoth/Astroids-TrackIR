5f858820c36625bee13b2829722c99d948fac143
Quinn Roth <qtroth2004@gmail.com>
2026-08-23 20:48:34 -0700

    Increase heat gain, max speed, and thrust scaler.

--- Assets/GameSpace/Spaceship/Scripts/SpaceshipMovement.cs
+++ Assets/GameSpace/Spaceship/Scripts/SpaceshipMovement.cs
@@ -31,12 +31,12 @@ public class SpaceshipMovement : MonoBehaviour
     public float speed { get; private set; }
 
     // Spaceship feel values
-    public float MaxSpeed = 300.0f;
+    public float MaxSpeed = 350.0f;
     public float LinearDamping = 2.0f;
     public float AngularDamping = 2.0f;
 
     public static float ThrustScaler = 40.0f;
-    public static float TIRThrustScaler = 120.0f; 
+    public static float TIRThrustScaler = 150.0f; 
     public static float PitchScaler = 1.2f;
     public static float YawScaler = 1.5f;
     public static float RollScaler = 1.1f;
